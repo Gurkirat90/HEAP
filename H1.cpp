b24ccf0abc807ca1b48870d5ced7c4bd3eb913f2
GURKIRAT <gs544448@gmail.com>
2024-11-10 23:34:21 +0530

    HEAPIFY

--- H1.cpp
+++ H1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+//BELOW FUNCTION IS TO HEAPIFY AS INGLE CELL THEREFORE->O(log n)
 void heapify(vector<int> &arr,int n,int i)
 {
   int largest=i;;
@@ -20,10 +21,10 @@ int main()
 {
   vector<int> arr={-1,54,53,52,50};
   int n=5;
-  for(i=n/2;i<0;i--)
+  for(i=n/2;i<0;i--)//----------------->O(n)
     {
-      heapify(arr,n,i);
+      heapify(arr,n,i);//----------------->O(log n )
     }
-  //this is build a heap therefore 
+  //this is build a heap therefore complexity should be O(n*log n)but is   O(n)
   
 }
